58597f6cd7697143280aa29bb5b03815b9a888a7
Knotten <kristian_knotten@hotmail.com>
2018-12-15 23:56:10 +0100

    endra mykje rart

---
+++ ComputerControll/app/src/main/java/knotten/kristian/computercontroll/ConnectToServer.java
@@ -0,0 +1,13 @@
+package knotten.kristian.computercontroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectToServer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_to_server);
+    }
+}
